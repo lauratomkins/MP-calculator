Fixed riming units

prospectus.py
@@ -17,6 +17,8 @@ e = moist_calc.eswFromTemp(dewp_C)  # vapor pressure
 shape = 'disk'
 relat = 'RY1989'
 initradius_m = md_calc.mass2radius(initmass_g, relat)
+#dep_colors = ['#01d298', '#087e5c', '#073325']
+dep_colors = ['#ffc1b9', '#ff948c', '#ff6361']
 
 dep_env = dict()
 
@@ -26,7 +28,8 @@ for ind, vpres in enumerate(e):
                            'e': vpres,
                            'esi': esi,
                            'p_kpa': p_kpa,
-                           'RHi': str(RHi[ind])}
+                           'RHi': str(RHi[ind]),
+                           'color': dep_colors[ind]}
 
 #%% riming environment:
 temp_C = -15 # deg C
@@ -41,6 +44,7 @@ relat = 'RY1989'
 initradius_m = md_calc.mass2radius(initmass_g, relat)
 lwc = np.array([0.5, 1.0, 2.0]) # g m-3
 rho_i = 0.9 # [g cm -3]
+rim_colors = ['#00c6e9','#008ec3','#075891']
 
 rim_env = dict()
 
@@ -53,7 +57,8 @@ for lw_ind, content in enumerate(lwc):
                            'lwc': content,
                            'rho_i': rho_i,
                            'coll_eff': 0.95,
-                           'RHi': str(RHi)}
+                           'RHi': str(RHi),
+                           'color': rim_colors[lw_ind]}
 
 #%% Grow drop by vapor deposition
 dep_drop = dict()
@@ -131,9 +136,9 @@ for lwc_env in rim_env.keys():
               iradius_m = md_calc.mass2radius(imass_g, relat)
 
               # calculate sublimation flux
-              iflux_kg = calculator.rimingFlux(env, iradius_m, fall_speed_ms,
+              iflux_g = calculator.rimingFlux(env, iradius_m, fall_speed_ms,
                                                grid=None, johnson_flag=True) # kg/s
-              iflux_g = iflux_kg * 1e3
+              #iflux_g = iflux_kg * 1e3
               sub_flux.append(iflux_g)
 
               # update drop mass
@@ -152,20 +157,23 @@ for lwc_env in rim_env.keys():
 fig = plt.figure(figsize=(6,6))
 ax1 = plt.gca()
 for key in dep_drop.keys():
-       ax1.plot(dep_drop[key]['time'], dep_drop[key]['drop_mass_g'] * 1e6,
-                label='RH$_{{ice}}$ = {0}%'.format(dep_env[key]['RHi']), linewidth=3)
+   ax1.plot(dep_drop[key]['time'], dep_drop[key]['drop_mass_g'] * 1e6,
+            label='RH$_{{ice}}$ = {0}%'.format(dep_env[key]['RHi']),
+            color=dep_env[key]['color'], linewidth=3)
 
 for key in rim_drop.keys():
-       ax1.plot(rim_drop[key]['time'], rim_drop[key]['drop_mass_g'] * 1e6,
-                label='LWC = {0} $g cm^{{-3}}$'.format(str(rim_env[key]['lwc'])), linewidth=3)
+   ax1.plot(rim_drop[key]['time'], rim_drop[key]['drop_mass_g'] * 1e6,
+            label='LWC = {0} g m$^{{-3}}$'.format(str(rim_env[key]['lwc'])),
+            color=rim_env[key]['color'], linewidth=3)
 
 ax1.set_ylabel('Mass [micrograms]')
 ax1.set_xlabel('Time [seconds]')
 ax1.set_yscale('log')
 ax1.set_xscale('log')
 ax1.set_xlim([1e0, 1e4])
-ax1.set_ylim([1e-2, 1e20])
-ax1.grid()
+ax1.set_ylim([1e-2, 1e7])
+ax1.grid(linestyle='--')
 #ax1.set_title('Riming sensitivity', loc='right')
 ax1.legend()
+#plt.savefig('Q:\\My Drive\\phd\\exams\\oral_exam\\figs\\mp_calc.png', dpi=600, bbox_inches='tight')
 plt.show()